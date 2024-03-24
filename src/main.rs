93100545d9191a9f3f69577d5f2ac43e373939b0
Syed Zia <46795709+szyth@users.noreply.github.com>
2024-03-25 00:05:44 +0530

    Stack switching

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+// Change the function's stack pointer to point to new function Hello in a new stack.
+// ret2libc wannabe
+// basically run a function without calling it in main() by playing with pointers
+use core::arch::asm;
+
+const SSIZE: isize = 48;
+
+#[derive(Debug, Default)]
+// using C language's ABI because it is very stable than Rust's
+#[repr(C)]
+struct ThreadContext {
+    rsp: u64,
+}
+
+fn hello() {
+    println!("Im a function who is run without being called.");
+    loop {}
+}
+
+unsafe fn gt_switch(new: *const ThreadContext) {
+    asm!(
+        "mov rsp, [{0} + 0x00]",
+        "ret",
+        in(reg) new,
+    )
+}
+
+fn main() {
+    let mut ctx = ThreadContext::default();
+    let mut stack = vec![0u8; SSIZE as usize];
+    unsafe {
+        let stack_bottom = stack.as_mut_ptr().offset(SSIZE);
+        let sb_aligned = (stack_bottom as usize & !15) as *mut u8;
+        std::ptr::write(sb_aligned.offset(-16) as *mut u64, hello as u64);
+        ctx.rsp = sb_aligned.offset(-16) as u64;
+
+        for i in 0..SSIZE {
+            println!("mem: {}, val: {}",
+            sb_aligned.offset(-i as isize) as usize,
+            *sb_aligned.offset(-i as isize))
+        }
+
+        
+        gt_switch(&mut ctx);
+    }
+}
